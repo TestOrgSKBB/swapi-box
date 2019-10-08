0594386282c6b86f7d7c9b0932ad038d1721f354
Zoe <zoekharvey@gmail.com>
2019-10-08 16:12:49 -0600

    Add onChange event to submit button

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -10,10 +10,18 @@ export default class Form extends Component {
     }
   }
 
+  submitUserInfo = (e) => {
+    e.preventDefault();
+    console.log('submit info firing')
+  }
+
   render() {
     return(
       <form>
-        <button>Submit</button>
+        <input type="text" placeholder="Enter Name"></input>
+        <input type="text" placeholder="Enter Favorite Star Wars Quote"></input>
+        
+        <button onClick={this.submitUserInfo}>Submit</button>
       </form>
     )
   }
