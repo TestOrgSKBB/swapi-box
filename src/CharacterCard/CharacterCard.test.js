9d4f4a6e655a8c568cbf8cbd00dd046bbd25d653
Zoe <zoekharvey@gmail.com>
2019-10-13 23:24:03 -0600

    Add snapshot tests

--- src/CharacterCard/CharacterCard.test.js
+++ src/CharacterCard/CharacterCard.test.js
@@ -5,6 +5,8 @@ import { isTSAnyKeyword, exportAllDeclaration } from '@babel/types';
 
 describe('CharacterCard', () => {
   let wrapper;
+  let mockUpdateFavorite = jest.fn()
+  let mockToggleFavorited = jest.fn()
   const mockCharacter = {
     films:['Tv: The Movie', 'A Movie About Musicals'],
     homeworld: 'Earth',
@@ -16,10 +18,24 @@ describe('CharacterCard', () => {
     wrapper = shallow(<CharacterCard
       key={100}
       character={mockCharacter}
+      updateFavorite={mockUpdateFavorite}
+      toggleFavorited={mockToggleFavorited}
       />)
   });
 
   it('should match the snapshot with all correct data', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should call toggleFavorite when button is clicked', () => {
+    wrapper = mount(<CharacterCard
+      key={100}
+      updateFavorite = {mockUpdateFavorite}
+      toggleFavorited = {mockToggleFavorited}
+      character={mockCharacter}
+      />)
+      wrapper.find('.article__favorite-button').simulate('click');
+      console.log(wrapper.props())
+      expect(mockToggleFavorited).toHaveBeenCalled()
+  })
 })
--- src/CharacterContainer/CharacterContainer.test.js
+++ src/CharacterContainer/CharacterContainer.test.js
@@ -6,12 +6,38 @@ import { isTSAnyKeyword, exportAllDeclaration } from '@babel/types';
 
 describe('CharacterContainer', () => {
   let wrapper
-  
-  it('should match the snapshot with all correct data', () => {
-    wrapper =
-      shallow(<CharacterContainer
-        characters={[]}
-        />)
-        expect(wrapper).toMatchSnapshot()
-      })
+ beforeEach(() => {
+   wrapper = shallow(<CharacterContainer
+    characters={[{
+      films:['A Film', 'Another Film', 'And another'],
+      homeworld: 'Earth',
+      name: 'A name',
+      population: '2000',
+      species:'human'
+    },
+    {
+      films:['A Film 2', 'Another Film 2', 'And another 2'],
+      homeworld: 'Earth 2',
+      name: 'A name 2',
+      population: '2000 2',
+      species:'human 2'
+    },
+    {
+      films:['A Film 3', 'Another Film 3', 'And another 3'],
+      homeworld: 'Earth 3',
+      name: 'A name 3',
+      population: '2000 3',
+      species:'human 3'
+    }]}
+    user={{
+      name: 'Dog',
+      quote: 'Bark',
+      ranking: 'Pretty good'
+    }}
+  />)
+ });
+
+ it('should match the snapshot with the correct data', () => {
+   expect(wrapper).toMatchSnapshot();
+ })
 })
