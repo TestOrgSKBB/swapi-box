60ddb6730ba07c0687caca521d5d9f15e64a4661
Zoe <zoekharvey@gmail.com>
2019-10-13 20:20:01 -0600

    Clean up commented out sections

--- src/App/App.js
+++ src/App/App.js
@@ -18,8 +18,6 @@ export default class App extends Component {
       name: '',
       quote: '',
       rank:'',
-      currentCharacters: [],
-      moreCount: 0
     }
   };
 
@@ -72,31 +70,6 @@ export default class App extends Component {
         const characters = this.state.movies.find(movie => movie.episode_id === parseInt(id)).characters
         return (<CharacterContainer updateState={this.updateState} opening_crawl={opening_crawl} characters={characters}/>)
       }} />
-
-      {/* --------   SAMPLE FOR CSS -------*/}
-{/* 
-      <section className="character-container">
-            <div className="star-wars">
-      <div className="div__crawl">
-      <p className="section__opening-crawl-test">It is a period of civil war.\n\nRebel spaceships, striking\n\nfrom a hidden base, have won\n\ntheir first victory against\n\nthe evil Galactic Empire.\n\n\n\nDuring the battle, Rebel\n\nspies managed to steal secret\r\nplans to the Empire's\n\nultimate weapon, the DEATH\n\nSTAR, an armored space\n\nstation with enough power\n\nto destroy an entire planet.\n\n\n\nPursued by the Empire's\n\nsinister agents, Princess\n\nLeia races home aboard her\n\nstarship, custodian of the\n\nstolen plans that can save her\n\npeople and restore\n\nfreedom to the galaxy....</p>
-      </div>
-      </div>
-    
-
-    <section className="section__character--cards">
-      <h5>Name: Luke Stkywalker</h5>
-      <h5>HomeWorld: The earth}</h5>
-      <h6>Population: 100,003}</h6>
-      <h5>Species: Human </h5>
-      <div>
-        <h6>Featured Films:</h6>
-        <h5>A LOT OF STUFF</h5>
-      </div>
-      <button>Favorite</button>
-    </section>
-    </section> */}
-
-{/* --------   SAMPLE FOR CSS -------*/}
     </main>
     )
   }
