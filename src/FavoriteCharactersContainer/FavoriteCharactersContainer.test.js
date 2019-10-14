514c2566823a87e2858af5be1babacb79a0a147e
Zoe <zoekharvey@gmail.com>
2019-10-14 17:12:43 -0600

    Add snapshot test for conditional render

--- src/FavoriteCharactersContainer/FavoriteCharactersContainer.test.js
+++ src/FavoriteCharactersContainer/FavoriteCharactersContainer.test.js
@@ -5,52 +5,41 @@ import CharacterCard from '../CharacterCard/CharacterCard';
 import { shallow } from 'enzyme';
 
 describe('FavoriteCharactersContainer', () => {
-  let wrapper
+  let wrapperHappy
+  let wrapperSad
   const mockUpdateFavorite = jest.fn()
   let mockCharacters = [
     {
       films:['A Film', 'Another Film', 'And another'],
-          homeworld: 'Earth',
-          name: 'A name',
-          population: '2000',
-          species:'human'
+      homeworld: 'Earth',
+      name: 'A name',
+      population: '2000',
+      species:'human'
     }
   ]
+  let mockNoCharacters = []
   
  beforeEach(() => {
-  wrapper= shallow(<FavoriteCharactersContainer 
+  wrapperHappy= shallow(<FavoriteCharactersContainer 
    key={100}
    characters={mockCharacters}
    updateFavorite={mockUpdateFavorite}
     />)
+  wrapperSad=shallow(<FavoriteCharactersContainer 
+    key={100}
+    characters={mockNoCharacters}
+    updateFavorite={mockUpdateFavorite}
+     />)
   })
 
+    it('should match the snapshot with the correct data', () => {
+      expect(wrapperHappy).toMatchSnapshot();
+    })
 
-  //  wrapper = shallow(<CharacterCard
-  //   characters={[{
-  //     films:['A Film', 'Another Film', 'And another'],
-  //     homeworld: 'Earth',
-  //     name: 'A name',
-  //     population: '2000',
-  //     species:'human'
-  //   },
-  //   {
-  //     films:['A Film 2', 'Another Film 2', 'And another 2'],
-  //     homeworld: 'Earth 2',
-  //     name: 'A name 2',
-  //     population: '2000 2',
-  //     species:'human 2'
-  //   },
-  //   {
-  //     films:['A Film 3', 'Another Film 3', 'And another 3'],
-  //     homeworld: 'Earth 3',
-  //     name: 'A name 3',
-  //     population: '2000 3',
-  //     species:'human 3'
-  //   }]}
-  // />)
-  it('should match the snapshot with the correct data', () => {
-    expect(wrapper).toMatchSnapshot();
-  })
+    it('should match the snapshot with the correct data', () => {
+      expect(wrapperSad).toMatchSnapshot();
+    })
+
+
  });
 
