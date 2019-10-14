45d6a2d6f919eba8f850b594d494d5872f2ee496
Zoe <zoekharvey@gmail.com>
2019-10-14 10:40:54 -0600

    Continue styling for movie page

--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -6,7 +6,7 @@ const MovieCard = (props) => {
   const { title, release_date, id, episode_id, selectMovie } = props;
   return (
     <section className="section__movie--card" id={episode_id} >
-      <h2 className="movie__card--element">{title}</h2>
+      <h2 className="movie__card--element movie__element--title">{title}</h2>
       <h3 className="movie__card--element">Episode {id}</h3>
       <h3 className="movie__card--element">{release_date}</h3>
       <NavLink className="movie__button--link" to={`movies/${id}/characters`}>
